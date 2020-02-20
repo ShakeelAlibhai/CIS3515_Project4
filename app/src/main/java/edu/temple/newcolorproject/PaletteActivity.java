2981be1eb781ddcc55bc4f23ee5292a4d74b7a49
Shakeel Alibhai <shakeel.alibhai419@gmail.com>
2020-02-20 09:12:17 -0500

    Add spinner (spinner logic is from Lab 3)

--- app/src/main/java/edu/temple/newcolorproject/PaletteActivity.java
+++ app/src/main/java/edu/temple/newcolorproject/PaletteActivity.java
@@ -1,8 +1,13 @@
 package edu.temple.newcolorproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 public class PaletteActivity extends AppCompatActivity {
 
@@ -10,5 +15,39 @@ public class PaletteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_palette);
+
+        final ConstraintLayout c = findViewById(R.id.layout);
+
+        Spinner s;
+        s = findViewById(R.id.spinner);
+
+        String colors[] = {
+                "Blue",
+                "Red",
+                "White",
+                "Yellow",
+                "Green",
+                "Aqua",
+                "Olive",
+                "Lime",
+                "Cyan",
+                "Silver"
+        };
+
+        ColorAdapter a = new ColorAdapter(this, colors);
+        s.setAdapter(a);
+
+        s.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                c.setBackgroundColor(Color.parseColor(parent.getItemAtPosition(position).toString()));
+                view.setBackgroundColor(Color.WHITE);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
