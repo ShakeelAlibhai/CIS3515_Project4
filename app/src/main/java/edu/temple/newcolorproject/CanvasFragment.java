6a2a415d682998d4d3a82bee9e5c862c74bab9b0
Shakeel Alibhai <shakeel.alibhai419@gmail.com>
2020-03-12 10:10:54 -0400

    Update layouts & work on CanvasFragment

--- app/src/main/java/edu/temple/newcolorproject/CanvasFragment.java
+++ app/src/main/java/edu/temple/newcolorproject/CanvasFragment.java
@@ -1,6 +1,7 @@
 package edu.temple.newcolorproject;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 
@@ -25,6 +26,28 @@ public class CanvasFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_canvas, container, false);
+        View v = inflater.inflate(R.layout.fragment_canvas, container, false);
+
+        //These are used to set the colors internally
+        //These names are not displayed to the user
+        //Note: The order of the colors here much match the order in strings.xml
+        String internalColors[] = {
+                "Blue",
+                "Red",
+                "White",
+                "Yellow",
+                "Green",
+                "Aqua",
+                "Olive",
+                "Lime",
+                "Cyan",
+                "Silver"
+        };
+
+        int colorIndex = 0; //To-do: Connect to PaletteActivity
+
+        v.setBackgroundColor(Color.parseColor(internalColors[colorIndex]));
+
+        return v;
     }
 }
