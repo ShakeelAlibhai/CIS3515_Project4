13173bd88fde81fffd3647ddd909bd06c823b22a
Shakeel Alibhai <shakeel.alibhai419@gmail.com>
2020-03-12 09:35:52 -0400

    Update layouts

--- app/src/main/java/edu/temple/newcolorproject/PaletteActivity.java
+++ app/src/main/java/edu/temple/newcolorproject/PaletteActivity.java
@@ -2,6 +2,8 @@ package edu.temple.newcolorproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Context;
 import android.content.Intent;
@@ -20,39 +22,46 @@ public class PaletteActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_palette);
 
-        final ConstraintLayout c = findViewById(R.id.layout);
+        PaletteFragment p = new PaletteFragment();
+        FragmentManager f = getSupportFragmentManager();
+        FragmentTransaction t = f.beginTransaction();
+        t.add(R.id.container1, p);
+        t.commit();
 
-        Spinner s;
-        s = findViewById(R.id.spinner);
-
-        Context context = getApplicationContext();
-
-        Resources res = context.getResources();
-        String colors[] = res.getStringArray(R.array.colors_array);
-
-        ColorAdapter a = new ColorAdapter(this, colors);
-        s.setAdapter(a);
-
-        s.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                if(!firstSelection) {   //Prevent onItemSelected() from running this code automatically (i.e. before the user chooses anything)
-                    Intent launchIntent = new Intent(PaletteActivity.this, CanvasActivity.class);
-//                    launchIntent.putExtra(INTENT_COLOR_KEY, parent.getItemAtPosition(position).toString());
-
-                    launchIntent.putExtra(INTENT_COLOR_KEY, position);
-                    startActivity(launchIntent);
-                } else {
-                    firstSelection = false;
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
+//        setContentView(R.layout.activity_palette);
+//
+//        final ConstraintLayout c = findViewById(R.id.layout);
+//
+//        Spinner s;
+//        s = findViewById(R.id.spinner);
+//
+//        Context context = getApplicationContext();
+//
+//        Resources res = context.getResources();
+//        String colors[] = res.getStringArray(R.array.colors_array);
+//
+//        ColorAdapter a = new ColorAdapter(this, colors);
+//        s.setAdapter(a);
+//
+//        s.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                if(!firstSelection) {   //Prevent onItemSelected() from running this code automatically (i.e. before the user chooses anything)
+//                    Intent launchIntent = new Intent(PaletteActivity.this, CanvasActivity.class);
+////                    launchIntent.putExtra(INTENT_COLOR_KEY, parent.getItemAtPosition(position).toString());
+//
+//                    launchIntent.putExtra(INTENT_COLOR_KEY, position);
+//                    startActivity(launchIntent);
+//                } else {
+//                    firstSelection = false;
+//                }
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//
+//            }
+//        });
     }
 }
